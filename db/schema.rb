9b72cef729e22bfc1d76ca72bcbb7ffea6e431cf
svergeylen <stephane@vergeylen.eu>
2017-09-17 17:16:51 +0200

    ajout nom au user mais pas de modif des views devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915130621) do
+ActiveRecord::Schema.define(version: 20170917150803) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170915130621) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
