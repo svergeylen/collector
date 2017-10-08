6d395f1b03824600bd130de7f2de67c0f29f5b39
svergeylen <stephane@vergeylen.eu>
2017-10-08 09:49:26 +0200

    Page auteurs

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,16 +1,43 @@
 class AuthorsController < ApplicationController
-  def index
-  end
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   def show
+    @author = Author.find(params[:id])
+    @series = Series.find( @author.items.collect(&:series_id).uniq! ).sort_by{ |s| s.name }
+
+    @colleague_ids = Itemauthor.where(item_id: @author.items.collect(&:id)).where.not(author_id: @author.id).collect(&:author_id).uniq!
+    @colleagues = Author.find(@colleague_ids).sort_by{ |a| a.name}
   end
 
   def create
   end
 
   def edit
+    @author = Author.find(params[:id])
   end
 
-  def delete
+  def update
+    @author = Author.find(params[:id])
+   
+    if @author.update(category_params)
+      redirect_to @author, notice: 'Auteur modifié'
+    else
+      render 'edit'
+    end
   end
+
+  def destroy
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+    def set_category
+      @author = Author.find(params[:id])
+    end
+
+  def category_params
+    params.require(:author).permit(:name)
+  end
+
 end
