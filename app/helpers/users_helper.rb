1d45fdd1fe4a6ec030d43f2e15789936ca4cde74
svergeylen <stephane@vergeylen.eu>
2017-10-17 18:18:28 +0200

    News: CSS pour un nouveau message et boutons prets pour emoji et attachments

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,10 +2,12 @@ module UsersHelper
 
 	# Returns avatar of the user in correct format for diplay next to messages, comments
 	def profile_picture(user)
-		return image_tag 	user.avatar.url(:tiny), 
+		link_to user do 
+			image_tag 	user.avatar.url(:tiny), 
 							alt: "Utilisateur: #{user.name}",
 							title: user.name,
 							class: 'profile-picture'
+		end
 	end
 
 	# Renvoie le nom d'un item récemment ajouté
