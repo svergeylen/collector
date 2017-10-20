e10645b71bd97207126094ef29c584925ddb2f9e
svergeylen <stephane@vergeylen.eu>
2017-10-20 12:21:40 +0200

    Affichage attachment en vue Galerie dans les series. Fixes #11

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,6 @@
 class Attachment < ApplicationRecord
 	belongs_to :element, polymorphic: true
 
-	# attr_accessible :image
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
