9f64bb6f25193169b16be4957da1d02d73da4523
svergeylen <stephane@vergeylen.eu>
2017-10-20 12:58:02 +0200

    mineur

---
+++ app/models/attachment.rb
@@ -0,0 +1,9 @@
+class Attachment < ApplicationRecord
+	belongs_to :element, polymorphic: true
+
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+	validates :user_id, presence: true
+
+end
