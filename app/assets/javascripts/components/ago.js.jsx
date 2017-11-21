d1ac52f82d0db7a2052c3f11e8f2a5849eaa4cdd
svergeylen <stephane@vergeylen.eu>
2017-11-21 19:03:29 +0100

    fix #57

--- app/assets/javascripts/components/ago.js.jsx
+++ app/assets/javascripts/components/ago.js.jsx
@@ -1,15 +1,14 @@
 
 var Ago = React.createClass({
 	getInitialState: function() {
+		var now = Math.round((new Date().getTime()/1000));
 		return {
-			now: Math.round((new Date().getTime()/1000)),
-			delta: NaN
+			now: now,
+			delta: (now - this.props.since)
 		}
 	},
 
 	componentDidMount() {
-		/* J'appelle tick uen première fois sinon les valeurs sont NaN... (pq ?) */
-		this.tick();
 	    this.timerID = setInterval(  () => this.tick(), 10000 );
 	},
 
@@ -30,13 +29,13 @@ var Ago = React.createClass({
 		var text = "";
 		var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 		var val = this.state.delta;
-		var ajd = new Date(this.props.since*1000);
-		var day = (ajd.getDate()  < 10) ? "0"+ajd.getDate()    : ajd.getDate();
-		var hour = ajd.getHours();
-		var minutes = (ajd.getMinutes()  < 10) ? "0"+ajd.getMinutes() : ajd.getMinutes();
-		var month = mois[ajd.getMonth()];
-		var title = "Le "+day+" "+month+" "+ajd.getFullYear()+", à "+hour+"h"+minutes;
-	
+		var since = new Date(this.props.since*1000);
+		var day = (since.getDate()  < 10) ? "0"+since.getDate()    : since.getDate();
+		var hour = since.getHours();
+		var minutes = (since.getMinutes()  < 10) ? "0"+since.getMinutes() : since.getMinutes();
+		var month = mois[since.getMonth()];
+		var title = "Le "+day+" "+month+" "+since.getFullYear()+", à "+hour+"h"+minutes;
+
 		/* Moins de une minute */
 		if (val < 60) {
 			text = "A l'instant !";
@@ -56,32 +55,34 @@ var Ago = React.createClass({
 					if (tmp > 1) { text += "s" }
 				}
 				else {
-					/* Vérification si le timestamp est encore d'aujourd'hui ou plus ancien */
-					
+					/* Création d'une variable fixe à minuit, pour voir si cela date d'aujourd'huir */
 					var minuit = new Date();
 					minuit.setHours(0,0,0,0);
+
+					/* Création d'un variable fixe hier à minuit, pour voir si c'est d'hier */
 					var hier_minuit = new Date();
 					hier_minuit.setHours(0,0,0,0);
-					hier_minuit.setDate(ajd.getDate() - 1);
+					hier_minuit.setDate(hier_minuit.getDate() - 1);
 					
-					/* C'est encore aujourd'hui */
-					if (ajd > minuit) {
+					/* C'est d'aujourd'hui */
+					if (since > minuit) {
 						text = "Aujourd'hui, à "+hour+"h"+minutes;
 					}
 					/* C'était avant minuit et donc hier */
 					else {
-						if (ajd > hier_minuit) {
+						if (since > hier_minuit) {
 							text = "Hier, à "+hour+"h"+minutes;
 						}
 						/* C'était avant hier minuit. On peut arrondir au jour près */
 						else {
+							/* Dans la semaine, on donne le nombre de jours depuis */
 							if (val < (7*24*3600)) {
 								tmp = Math.round(val/(3600*24));
 								text = "Il y a "+tmp+" jour";
 								if (tmp > 1) {text+= "s"; }
 							}
 							else {
-								text = title;
+								text = "Le "+day+" "+month+" "+since.getFullYear();
 							}
 						}
 					}
