88400d258e2cffac2aec238c0d5a8d4a75d4f2bb
svergeylen <stephane@vergeylen.eu>
2017-11-22 17:26:05 +0100

    ajout bouton haut de page. fix #40

---
+++ app/assets/javascripts/global.js
@@ -0,0 +1,21 @@
+
+document.addEventListener("turbolinks:load", function() {
+	var offset = 500;
+	var duration = 800;
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > offset) {
+			$(".back-to-top").fadeIn(duration);
+		} 
+		else {
+			$(".back-to-top").fadeOut(duration);
+		}
+	});
+
+	$(".back-to-top").click(function(event) {
+ 		event.preventDefault();
+		$("html, body").animate({scrollTop: 0}, duration);
+		return false;
+	});
+
+});
