4aea1b862400d319e2379a65a1612a6def9c0c15
svergeylen <stephane@vergeylen.eu>
2017-12-29 12:16:57 +0100

    ajout colonnes fixture optional dans tags et rails_view dans items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227164358) do
+ActiveRecord::Schema.define(version: 20171229110925) do
 
   create_table "attachments", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20171227164358) do
     t.integer "adder_id"
     t.float "number"
     t.text "description"
+    t.integer "rails_view", default: 0
   end
 
   create_table "items_tags", id: false, force: :cascade do |t|
@@ -121,6 +122,8 @@ ActiveRecord::Schema.define(version: 20171227164358) do
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.integer "category_id"
+    t.boolean "fixture", default: false
+    t.boolean "optional", default: false
   end
 
   create_table "users", force: :cascade do |t|
