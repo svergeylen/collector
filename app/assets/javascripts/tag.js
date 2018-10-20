b64b54146a46c9226ec595eaa9062de0e8027299
Stéphane <stephane@vergeylen.eu>
2018-10-20 17:41:50 +0200

    Sélection des items en liste et en galerie pour travail en masse
    le checkbox est masqué à présent car le clic sur une ligne ou sur une card active ou désactive le checkbox correspondant.
    l'touil sélectionner tout inverse la sélection courante (donc sélectionne tout si rien n'est sélectionné)

--- app/assets/javascripts/tag.js
+++ app/assets/javascripts/tag.js
@@ -10,4 +10,32 @@ document.addEventListener("turbolinks:load", function() {
 		}
 	});
 
+	/* Clic sur une card de la galerie ou d'une ligne dans la liste doit cocher le checkbox de l'item correspondant */
+	$(".collector-item-li").click(function(e){
+		toggle($(this));
+	});
+
+	/* Coche tous les items de la liste ou de la galerie */
+	$("#check_all").click(function(e) {
+		e.preventDefault();
+		objects = $(".collector-item-li");
+		$.each( objects, function( index, elem ) {
+		  toggle(elem);
+		});
+	});
+
 });
+
+/* Coche le checkbox contenu dans la ligne de la liste ou dans la card de la gallerie */
+function toggle(elem) {
+	var item_id = $(elem).data("item-id");
+	checkbox = $("#item_"+item_id);
+	if (checkbox.prop("checked")) {
+		checkbox.prop("checked", false);
+		$(elem).removeClass("checked");
+	}
+	else {
+		checkbox.prop("checked", true);
+		$(elem).addClass("checked");
+	}
+}
