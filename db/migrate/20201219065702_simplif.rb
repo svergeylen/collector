3bc433dc0e89c4d1cc386de12a0d7c6d2e58dd30
Stéphane <stephane@vergeylen.eu>
2020-12-19 09:06:54 +0100

    rails  de merde

--- db/migrate/20201219065702_simplif.rb
+++ db/migrate/20201219065702_simplif.rb
@@ -6,10 +6,10 @@ class Simplif < ActiveRecord::Migration[5.1]
   	#remove_column :tags, :filter_items
   	#drop_table :favourites
   	#Ownertag.where(owner_type: "Tag").delete_all
-  	#remove_column :ownertags, :owner_type
-  	#remove_column :ownertags, :id
-  	#rename_column :ownertags, :owner_id, :item_id
-  	#rename_table :ownertags, :items_tags
+  	remove_column :ownertags, :owner_type
+  	remove_column :ownertags, :id
+  	rename_column :ownertags, :owner_id, :item_id
+  	rename_table :ownertags, :items_tags
 		# Purge anciens votes sur items et comments (act_as_votable)
 		#results = ActiveRecord::Base.connection.execute("DELETE FROM votes WHERE votable_type = 'Item' ")
 		#results = ActiveRecord::Base.connection.execute("DELETE FROM votes WHERE votable_type = 'Comment' ")
