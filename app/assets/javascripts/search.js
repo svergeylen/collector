3bd47c850debdd243ea644742d7382941c71d103
Stéphane <stephane@vergeylen.eu>
2021-02-21 18:11:28 +0100

    focus search result 1

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,6 +2,6 @@
 document.addEventListener("turbolinks:load", function() {
 
 	/* Sélection du premier élément de la liste des résultats de search gobal */
-	$("div.search_results a:first").focus();
+	$(".search_results ul li a:first").focus();
 
 });
